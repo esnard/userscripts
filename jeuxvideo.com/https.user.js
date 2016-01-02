37d8110a75e8957b6f0e068fe001ef1770bf3cb2
Benoit Esnard <benoit.esnard@gmail.com>
2016-01-02 23:39:22 +0100

    JeuxVideo.com : redirects HTTP requests to HTTPS

---
+++ jeuxvideo.com/https.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        HTTPS
+// @namespace   https://github.com/esnard/userscripts
+// @version     1.0
+// @description JeuxVideo.com : redirects HTTP requests to HTTPS.
+// @author      Benoit Esnard
+// @match       http://www.jeuxvideo.com/*
+// @grant       none
+// ==/UserScript==
+
+(function () {
+    var url = new URL(window.location);
+    url.protocol = "https:";
+    window.location = url;
+})();

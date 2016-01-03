6575d8c69a5731285d8f1b1e17e8fe9ae97ac3ff
Benoit Esnard <benoit.esnard@gmail.com>
2016-01-03 23:49:52 +0100

    Fix typo

--- jeuxvideo.com/https.user.js
+++ jeuxvideo.com/https.user.js
@@ -2,7 +2,7 @@
 // @name        HTTPS
 // @namespace   https://github.com/esnard/userscripts
 // @version     1.0
-// @description JeuxVideo.com : redirects HTTP requests to HTTPS.
+// @description JeuxVideo.com: redirects HTTP requests to HTTPS.
 // @author      Benoit Esnard
 // @match       http://www.jeuxvideo.com/*
 // @grant       none

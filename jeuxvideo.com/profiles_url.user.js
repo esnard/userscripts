e29f2570e5daf460fa4794f7ae6d063b0a470153
Benoit Esnard <benoit.esnard@gmail.com>
2016-01-05 23:56:55 +0100

    Redirects profiles with a captalized letter to the correct URL

---
+++ jeuxvideo.com/profiles_url.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Profiles URLs
+// @namespace   https://github.com/esnard/userscripts
+// @version     1.0
+// @description Allows capitalized letters in profiles URLs.
+// @author      Benoit Esnard
+// @match       http://www.jeuxvideo.com/profil/*
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+(function () {
+    var pathname = window.location.pathname.toLowerCase();
+    if (window.location.pathname !== pathname) {
+        window.location.pathname = pathname;
+    }
+})();

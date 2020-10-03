05f589f4de208a366cf334ebe3c0fb4716b11a4e
quechi199900 <71445529+quechi199900@users.noreply.github.com>
2020-10-04 05:58:25 +0700

    Update and rename facebook/autopoker.user.js to Facebook/autopoker.user.js
    Việt Nam

---
+++ Facebook/autopoker.user.js
@@ -0,0 +1,69 @@
+// ==UserScript== https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+// @name        Facebook Nguyễn Phú Lộc
+// @namespace   https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+// @version     1.0
+// @description Automatically pokes back.
+// @author      Benoit Esnard
+// @match       https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+// @grant       none
+// ==/UserScript== https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+
+(function () {
+    var getQueryStringParams = function (url) { https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+        if (typeof url !== "string") {
+            url = url.toString(); https://m.facebook.com/phuloc.196?tsid=0.3434953298030796
+        }
+
+        var params = {};
+        var queryStringOffset = url.toString().indexOf("?");
+
+        if (queryStringOffset < 0) {
+            return params;
+        }
+
+        var queryString = url.substr(queryStringOffset + 1);
+
+        queryString.split("&").forEach(function (part) {
+            var separatorOffset = part.indexOf("=");
+
+            if (separatorOffset < 0) {
+                return;
+            }
+
+            var paramName  = part.substr(0, separatorOffset);
+            var paramValue = part.substr(separatorOffset + 1);
+
+            params[paramName] = paramValue;
+        });
+
+        return params;
+    };
+
+    var target   = document.getElementById("contentArea");
+    var options  = {childList: true, subtree: true};
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            [].forEach.call(mutation.addedNodes, function (node) {
+                if (!(node instanceof HTMLElement)) {
+                    return;
+                }
+
+                [].forEach.call(node.getElementsByTagName("a"), function (link) {
+                    var ajaxify = link.getAttribute("ajaxify");
+
+                    if (!ajaxify) {
+                        return;
+                    }
+
+                    var params = getQueryStringParams(ajaxify);
+
+                    if (params.is_hide === "0" && params.poke_target) {
+                        link.click(); https://m.facebook.com/phuloc.196?tsid=0.3434953298030796&source=result
+                    }
+                });
+            });
+        });
+    });
+
+    observer.observe(target, options);
+})();
